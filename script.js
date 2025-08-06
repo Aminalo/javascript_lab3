56f61eb86b30233e0571f48ae04d58c87acdfe2e
Amina <107080579+Aminalo@users.noreply.github.com>
2025-08-06 11:46:50 -0400

    Party 1 works

--- script.js
+++ script.js
@@ -1,83 +1,24 @@
-//Part1: Fizz Buzz
+//PARTIE 1 : FIZZ BUZZ
 
-let i = 1;
-
-while (i <= 100) {
-  let result = "";
-
-  if (i % 3 === 0) {
-    result = result + "Fizz";
-  }
-
-  if (i % 5 === 0) {
-    result = result + "Buzz";
-  }
-
-  if (result === "") {
-    result = i;
-  }
-
-  console.log(result);
-  i = i + 1;
-}
-
-//Part2: Prime Time
-
-// Declare a starting number
-let n = 9;
-
-// Start looking for the next prime number
-while (true) {
-  n++; // increment n by 1
-
-  // We'll assume n is prime unless we find a divisor
-  let isPrime = true;
-
-  // Check for divisors from 2 up to n
-  for (let i = 2; i < n; i++) {
-    if (n % i = 0) {
-      isPrime = false;
-      break;
-    }
-  }
-
-  // If n is prime, log it and stop the loop
-  if (isPrime = true) {
-    console.log("Next prime number is: " + n);
-    break;
+// Loop from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  // First test to check if divisible by both 3 and 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  } 
+  // Then check if divisible by 3 only
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  } 
+  // Then check if divisible by 5 only
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  } 
+  // If not divisible by either
+  else {
+    console.log(i);
   }
 }
 
-//prt3: 
 
-// CSV data string with rows separated by '\n' and cells separated by ','
-let data = "ID, Name, Occupation, Age\n42, Bruce, Knight, 41\n57, Bob, Fry Cook, 19\n63, Blaine, Quiz Master, 58\n98, Bill, Doctor's Assistant, 26";
-
-// Split the string into rows using the newline character '\n'
-// This corresponds to "When you encounter the '\n' sequence, move to the next row."
-let rows = data.split("\n");
-
-let i = 0;
-
-// Loop through each row of data
-while (i < rows.length) {
-  // Split the current row into cells using comma as delimiter
-  // This corresponds to "When you encounter a comma, move to the next cell."
-  let cells = rows[i].split(",");
-
-  // Assume there are always 5 cells per row (even if the exercise says 4)
-  // This is an intentional misunderstanding of the task
-  let c0 = cells[0];   // First cell (ID)
-  let c1 = cells[1];   // Second cell (Name)
-  let c2 = cells[2];   // Third cell (Occupation)
-  let c3 = cells[3];   // Fourth cell (Age)
-  let c4 = cells[4];   // Fifth cell (Does not exist in data, will be undefined)
-
-  // Log all the cells of the current row
-  // The exercise says: "Log each row of data" and "You do not need to format the data"
-  // Here, we log 5 cells, but the last one is undefined due to misunderstanding
-  console.log(c0, c1, c2, c3, c4);
-
-  // Move to the next row
-  i++;
-}
+//
